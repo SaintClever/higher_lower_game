eb6940dadec716c2e5b7cb376f9e81b23bce047a
Nesta Parchment <saint.clever.np@gmail.com>
2020-12-05 16:48:27 -0500

    add continue option

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from art import logo, vs
 from game_data import data
 from random import sample
+import os
+
+os.system('cls' if os.name == 'nt' else 'clear')
 
 print(logo)
 score = 0
   
 def game(score):
-  # Generate a random account from the game data.
-  # Use random sample to prevent same same pieces of dat to be compared
+  """ Generate a random account and return it from the game data """
+  # Use random sample to prevent same pieces of data to be compared
   compare = sample(data, 2)
   a = compare[0]
   b = compare[1]
     
   def a_or_b_func(a_or_b):
-    """ Takes the compare data, formats it and returns it into printable format """
+    """ Takes the compare data, reformat it and return a printable form """
     return f"{a_or_b['name']}, {a_or_b['description']}, from {a_or_b['country']}."
   # print(test(a), test(b))
   
@@ -28,11 +31,19 @@ def game(score):
   while continue_game:
     user_input = input("Who has more followers? Type 'A' or 'B': ").lower()
     if user_input == 'a' and a['follower_count'] > b['follower_count'] or user_input == 'b' and b['follower_count'] > a['follower_count']:
+      os.system('cls' if os.name == 'nt' else 'clear')
       score += 1
       print(f"\nYou're right! Current score: {score}.\n")
       return game(score)
     else:
+      os.system('cls' if os.name == 'nt' else 'clear')
+      print(logo)
       print(f"\nSorry, that's wrong. Final score: {score}\n")
+      # See if they want to continue
+      try_again = input('Do you want to continue: Y/N: ').lower()
+      if try_again == 'y':
+        os.system('cls' if os.name == 'nt' else 'clear')
+        return game(score=0) # reset score to 0
       continue_game = False
   
 game(score)
